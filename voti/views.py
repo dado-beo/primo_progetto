ac66cd0c04b6b06f055c1ea2166c8fb354ed5cff
dado-beo <123021686+dado-beo@users.noreply.github.com>
2026-02-16 13:53:52 +0100

    Merge

---
+++ voti/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+materie = ["Matematica","Italiano","Inglese","Storia","Geografia"]
+
+def view_a(request, materie):
+    for i in materie:
+        i 
+    return render(request)
+
+def view_b(request):
+    return render(request)
+
+def view_c(request):
+    return render(request)
+
+def view_d(request):
+    return render(request)
